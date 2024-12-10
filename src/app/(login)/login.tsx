80c98fb67967ee9afd7703329ff866538f3c2ba3
Reeshabh Jain <jain2001.rishabh@gmail.com>
2024-12-10 20:53:29 +0530

    minor changes

--- src/app/(login)/login.tsx
+++ src/app/(login)/login.tsx
@@ -1,14 +1,14 @@
-import { useSearchParams } from "next/navigation";
-import { useState } from "react"; 
+// import { useSearchParams } from "next/navigation";
+// import { useState } from "react"; 
 
-export function Login({ mode = "signup"}: { mode?: "signin" | "signup" }) {
-    const [loading, setLoading] = useState(false)
-    const searchParams = useSearchParams()
-    const redirect = searchParams.get("redirect")
-    const priceId = searchParams.get("priceId")
-    const discountCode = searchParams.get("discountCode")
+// export function Login({ mode = "signup"}: { mode?: "signin" | "signup" }) {
+//     // const [loading, setLoading] = useState(false)
+//     // const searchParams = useSearchParams()
+//     // const redirect = searchParams.get("redirect")
+//     // const priceId = searchParams.get("priceId")
+//     // const discountCode = searchParams.get("discountCode")
 
-    // const handleGoogleSignIn = () => {
-    //     const redirectTo = `${config.domainName}/`
-    // }
-}
+//     // const handleGoogleSignIn = () => {
+//     //     const redirectTo = `${config.domainName}/`
+//     // }
+// }
